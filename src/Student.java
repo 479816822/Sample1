18ead8867c954f54ebedca659b37016a8b41322b
479816822 <479816822@qq.com>
2016-04-23 10:27:21 +0800

    Student teacher

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+
+
+/**
+ * Student这一个类其实就是对一个现实中的一个学生的完整的抽象
+ * 其中的方法就是一个学生的动作，做什么
+ * 属性就是他特有的基本特点
+ * 公有的属性就是所有的学生党相同
+ * 私有属性就是每一个学生自己独一无二的
+ * 保护属性就是每一个的区别
+ * @author NIIT
+ *
+ */
+public class Student {
+	//学生私有的属性
+	private String stuName=null;
+	private int stuAge=-1;
+	private String stuSex=null;
+	private int stuGrand=-1;    //学生的年级
+	
+	//
+	public Student(String name,int age,String Sex,int Grand)
+	{
+		stuName=name;
+		stuAge=age;
+		stuSex=Sex;
+		stuGrand=Grand;
+		
+		
+	}
+	
+
+}
---
+++ src/Teacher.java
@@ -0,0 +1,17 @@
+
+public class Teacher {
+	private String teaName=null;
+	private int   teaNo=-1;
+	private int teaAge=-1;
+	private int teaYear=-1;
+	
+	public Teacher(String name,int no,int age,int year)
+	{
+		teaAge=age;
+		teaName=name;
+		teaNo=no;
+		teaYear=year;
+		
+	}
+
+}
